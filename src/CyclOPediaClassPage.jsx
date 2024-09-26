1d940b953ebf911579e83e3863e9c45e7b36ecb3
Md Al Amin <al.amin@bizzntek.com>
2024-09-26 14:29:28 +0600

    api.jsx file added and axios install

---
+++ src/CyclOPediaClassPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class CyclOPediaClassPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      instructor: undefined,
+      studentList: [],
+      studentCount: 0,
+      hideInstructor: false,
+    };
+  }
+  render() {}
+}
