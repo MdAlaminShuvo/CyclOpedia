20918f08c965d24608cbcde0ad23efc6b953e2b2
Md Al Amin <al.amin@bizzntek.com>
2024-09-26 16:45:30 +0600

    Controlled component are added

--- src/CyclOPediaClassPage.jsx
+++ src/CyclOPediaClassPage.jsx
@@ -8,6 +8,8 @@ class CyclOPediaClassPage extends React.Component {
       studentList: [],
       studentCount: 0,
       hideInstructor: false,
+      inputName: "",
+      inputFeedback: "",
     };
   }
   componentDidMount = async () => {
@@ -62,9 +64,31 @@ class CyclOPediaClassPage extends React.Component {
           </div>
         )}
         <div className="p-3">
+          <span className="h4 text-success">Feedback</span>
+          <br />
+          <input
+            type="text"
+            value={this.state.inputName}
+            placeholder="Name..."
+            onChange={(e) => {
+              this.setState({ inputName: e.target.value });
+            }}
+          ></input>{" "}
+          Value : {this.state.inputName}
+          <br />
+          <textarea
+            value={this.state.inputFeedback}
+            placeholder="Feedback..."
+            onChange={(e) => {
+              this.setState({ inputFeedback: e.target.value });
+            }}
+          ></textarea>{" "}
+          Value : {this.state.inputFeedback}
+        </div>
+        <div className="p-3">
           <span className="h4 text-success">Students</span>
           <br />
-          <div>Student Count :{this.state.studentCount}</div>
+          <div>Student Count :{this.state.studentCont}</div>
           <button
             className="btn btn-success btn-sm"
             onClick={this.handleAddStudent}
